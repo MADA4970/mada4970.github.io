a41b2ddb5c1345e68320f287c23412a5baafc10d
Marco <marco.davila@colorado.edu>
2025-03-19 23:45:25 -0600

    added a night mode

--- ica/ica10/ica10.js
+++ ica/ica10/ica10.js
@@ -2,8 +2,11 @@ function showMessage(){
     let outputElement = document.getElementById('output')
    
     if(outputElement.innerText == 'Inertia is a property of matter'){
-        outputElement.innerText = 'BILL BILL BILL'
+        outputElement.innerText = 'BILL BILL BILL BILL'
     }else{
         outputElement.innerText = 'Inertia is a property of matter' 
     }
+}
+function goodNightMode(){
+    document.body.classList.toggle(('goodNight'))
 }
