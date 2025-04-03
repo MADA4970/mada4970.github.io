acc089b8d038d512a35b56659f698504356278df
Marco <marco.davila@colorado.edu>
2025-04-03 16:05:07 -0600

    added my pics to the right folder

--- wa/wa11/main.js
+++ wa/wa11/main.js
@@ -5,9 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const gallery = [pic1.jpg, pic2.jpg, pic3.jpg,pic4.jpg,pic5.jpg]
 
 /* Declaring the alternative text for each image file */
-
+const altText = {
+    'pic1.jpg': 'A man with an uncanny valley face',
+    'pic2.jpg':'A person trying out uncanny valley makeup',
+    'pic3.jpg': 'The blonde kid from Polar Express looking back over his seat',
+    'pic4.jpg':'A strange collection of masks',
+    'pic5.jpg': 'a painted woman being forced to smile'
+}
 /* Looping through images */
 
 const newImage = document.createElement('img');
