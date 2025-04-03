d8adfa67bd96e2865026dabb84db3f16560d3f5c
Marco <marco.davila@colorado.edu>
2025-04-03 16:45:49 -0600

    in progress

--- wa/wa11/main.js
+++ wa/wa11/main.js
@@ -5,21 +5,37 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const gallery = [pic1.jpg, pic2.jpg, pic3.jpg,pic4.jpg,pic5.jpg]
+const gallery = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg','pic4.jpg','pic5.jpg']
 
 /* Declaring the alternative text for each image file */
 const altText = {
     'pic1.jpg': 'A man with an uncanny valley face',
-    'pic2.jpg':'A person trying out uncanny valley makeup',
+    'pic2.jpg':'AI robot with an uncanny valley face',
     'pic3.jpg': 'The blonde kid from Polar Express looking back over his seat',
     'pic4.jpg':'A strange collection of masks',
     'pic5.jpg': 'a painted woman being forced to smile'
 }
 /* Looping through images */
+for(const image of gallery){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src',`images/${image}` );
+    newImage.setAttribute('alt', altText[image]);
+    thumbBar.appendChild(newImage);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+     newImage.addEventListener('click', e =>{
+        displayedImage.setAttribute('src', e.target.src);
+        displayedImage.setAttribute('alt',e.target.alt);
+     })
+}
 
 /* Wiring up the Darken/Lighten button */
+const btnClass = btn.getAttribute('class');
+if(btnClass === 'dark'){
+    btn.setAttribute("class", light);
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = "rgb(0 0 0 / 50%)"; 
+}else{
+    btn.setAttribute("class", dark);
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0%)"; 
+}
